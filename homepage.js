f3e6a944e2ee8f413c1d5cb6c0cd8b4839e4f5fd
EvanderGibson <evanderbg2@gmail.com>
2024-09-10 13:55:35 -0700

    created salvador dali page

--- homepage.js
+++ homepage.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
 
-<<<<<<< HEAD
+
 	$(".about") .click(function() {
 		$(".abouttext ").toggle("fast");
 		
 	});
 
-=======
->>>>>>> 79ac40a9848f777b5a33e35ee26991d9360fa2ff
+
 });
 
