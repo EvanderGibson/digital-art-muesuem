ebd78505c0652a8653464e74f4c6662654dacffa
DvidChavez <chavezdavid0609@gmail.com>
2024-09-11 13:17:07 -0700

    background image

--- homepage.js
+++ homepage.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
 
-<<<<<<< HEAD
 	$(".about") .click(function() {
-		$(".abouttext ").toggle("fast");
-		
+		$(".abouttext ").css("visibility", "visible");
 	});
-
-=======
->>>>>>> 79ac40a9848f777b5a33e35ee26991d9360fa2ff
 });
 
