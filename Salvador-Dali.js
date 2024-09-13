c537d4912e322fea5e0146bbddd675800cd0fecb
EvanderGibson <evanderbg2@gmail.com>
2024-09-13 13:02:49 -0700

    gallery page for salvador dali

--- Salvador-Dali.js
+++ Salvador-Dali.js
@@ -1,1 +1,18 @@
+$(document).ready(function(){
 
+	$(".old") .click(function() {
+		$(".info1 ").toggle("fast");
+	});
+	
+	$(".bigblue") .click(function() {
+		$(".info2 ").toggle("fast");
+	});
+	
+	$(".woman") .click(function() {
+		$(".info3 ").toggle("fast");
+	});
+	
+	$(".moon") .click(function() {
+		$(".info4 ").toggle("fast");
+	});
+});
