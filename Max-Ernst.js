3d62645be12895a2f9f9cc5087f0334bd221086c
EvanderGibson <evanderbg2@gmail.com>
2024-09-17 13:39:11 -0700

    descriptions for magritte and ernst

--- Max-Ernst.js
+++ Max-Ernst.js
@@ -1,1 +1,18 @@
+$(document).ready(function(){
 
+	$(".hat") .click(function() {
+		$(".info1 ").toggle("fast");
+	});
+	
+	$(".fat") .click(function() {
+		$(".info2 ").toggle("fast");
+	});
+	
+	$(".angel") .click(function() {
+		$(".info3 ").toggle("fast");
+	});
+	
+	$(".swimma") .click(function() {
+		$(".info4 ").toggle("fast");
+	});
+});
