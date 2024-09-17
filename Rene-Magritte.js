4c7d5b1fbad0d99ff6ea2f6344988108089377e9
EvanderGibson <evanderbg2@gmail.com>
2024-09-17 12:51:00 -0700

    started ersnt finished magritte

--- Rene-Magritte.js
+++ Rene-Magritte.js
@@ -1,1 +1,18 @@
+$(document).ready(function(){
 
+	$(".train") .click(function() {
+		$(".info1 ").toggle("fast");
+	});
+	
+	$(".rock") .click(function() {
+		$(".info2 ").toggle("fast");
+	});
+	
+	$(".ball") .click(function() {
+		$(".info3 ").toggle("fast");
+	});
+	
+	$(".sneak") .click(function() {
+		$(".info4 ").toggle("fast");
+	});
+});
