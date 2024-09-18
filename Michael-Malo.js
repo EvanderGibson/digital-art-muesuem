48d83d6be406a2dcfdf94d0a05cff955b92f82af
DvidChavez <chavezdavid0609@gmail.com>
2024-09-18 13:15:32 -0700

    micheal vincent manalo page

--- Michael-Malo.js
+++ Michael-Malo.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
 
-	$(".ocean") .click(function() {
+	$(".ocean").click(function() {
 		$(".info4 ").toggle("fast");
 	});
 	
+	$(".pinkcity").click(function() {
+		$(".info3 ").toggle("fast");
+	});
+	
+	$(".libary").click(function() {
+		$(".info2 ").toggle("fast");
+	});
+	
+	$(".nightcity").click(function() {
+		$(".info1 ").toggle("fast");
+	});
+
 });
 
